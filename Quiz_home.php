47b5fec3c4c04974235dc120a0b6afb7f770f901
増田健太 <masudakenta@masudakentanoMacBook-Pro.local>
2020-07-06 21:12:27 +0900

    Home.php を初めてコミット

---
+++ Quiz_home.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>安全保障クイズ</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.bundle.min.js"></script>
+</head>
+<body>
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <a class="navbar-brand" href="#">CISTEC テストアプリ</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar-sample">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbar-sample">
+    <ul class="navbar-nav">
+      <li class="nav-item">
+        <a class="nav-link" href="#">Home</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Add</a>
+      </li>
+    </ul>
+  </div>
+</nav>
+
+    
+</body>
+</html>
